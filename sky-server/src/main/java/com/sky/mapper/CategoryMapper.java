d7824d3811e369eb82d6ecb531c25d3bfe099186
RogerJ <827090192@qq.com>
2025-05-08 23:51:02 +0800

    完成分类管理所有接口

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,35 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    Page<Category> getCategoryList(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    @Insert("insert into category (type, name, sort, status, create_time, update_time, create_user, update_user) " +
+            "values (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser});")
+    int addCategory(Category category);
+
+    @Select("select count(1) from category where name = #{name}")
+    int getCategoryByName(String name);
+
+    @Select("select * from category where id = #{id}")
+    Category getCategoryById(Long id);
+
+    int updateCategory(Category category);
+
+    @Delete("delete from category where id = #{id}")
+    int delCategoryById(Long id);
+
+    @Select("select * from category where type = #{type}")
+    List<Category> listCategoryByType(Integer type);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,106 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.exception.BusinessException;
+import com.sky.mapper.CategoryMapper;
+import com.sky.result.PageResult;
+import com.sky.service.CategoryService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryMapper.getCategoryById(id);
+    }
+
+    @Override
+    public int getCategoryByName(String categoryName) {
+        return categoryMapper.getCategoryByName(categoryName);
+    }
+
+    @Override
+    public boolean changeCategoryStatus(Long id, Integer status) {
+        //查找id是否存在
+        Category category = getCategoryById(id);
+        if (category == null) throw new BusinessException("分类ID不存在");
+
+        category.setStatus(status);
+        category.setUpdateTime(LocalDateTime.now());
+        category.setUpdateUser(BaseContext.getCurrentId());
+
+        int affectRow = categoryMapper.updateCategory(category);
+
+        return affectRow > 0;
+    }
+
+    @Override
+    public List<Category> listCategoryByType(Integer type) {
+        return categoryMapper.listCategoryByType(type);
+    }
+
+    @Override
+    public PageResult<Category> getCategoryList(CategoryPageQueryDTO categoryPageQueryDTO) {
+        Page<Category> categoryPage = PageHelper.startPage(categoryPageQueryDTO.getPage(), categoryPageQueryDTO.getPageSize())
+                .doSelectPage(() -> categoryMapper.getCategoryList(categoryPageQueryDTO));
+        return new PageResult<>(categoryPage.getTotal(), categoryPage.getResult(), categoryPage.getPageSize(), categoryPage.getPageNum());
+    }
+
+    @Override
+    public boolean addCategory(CategoryDTO categoryDTO) {
+        // 分类名称要唯一
+        // 根据分类名称查找分类
+        int count = getCategoryByName(categoryDTO.getName());
+        if (count > 0) throw new BusinessException("分类名称已存在，请重新输入");
+
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO, category);
+        category.setStatus(StatusConstant.DISABLE);
+        category.setCreateTime(LocalDateTime.now());
+        category.setUpdateTime(LocalDateTime.now());
+        category.setCreateUser(BaseContext.getCurrentId());
+        category.setUpdateUser(BaseContext.getCurrentId());
+
+        int affectRow = categoryMapper.addCategory(category);
+        return affectRow > 0;
+    }
+
+    @Override
+    public boolean updateCategory(CategoryDTO categoryDTO) {
+        //查找id是否存在
+        Category category = getCategoryById(categoryDTO.getId());
+        if (category == null) throw new BusinessException("分类ID不存在");
+
+        BeanUtils.copyProperties(categoryDTO, category);
+        category.setUpdateUser(BaseContext.getCurrentId());
+        category.setUpdateTime(LocalDateTime.now());
+
+        int affectRow = categoryMapper.updateCategory(category);
+        return affectRow > 0;
+    }
+
+    @Override
+    public boolean delCategory(Long id) {
+        //查找id是否存在
+        Category category = getCategoryById(id);
+        if (category == null) throw new BusinessException("分类ID不存在");
+
+        int affectRow = categoryMapper.delCategoryById(id);
+        return affectRow > 0;
+    }
+}
