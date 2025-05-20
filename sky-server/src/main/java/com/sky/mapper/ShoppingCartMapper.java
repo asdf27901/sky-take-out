a3653ca9951ba9ebf55ba99f5245170ca450654a
RogerJ <827090192@qq.com>
2025-05-20 23:23:05 +0800

    Feature: 完成购物车列表、清空购物车、删除购物车接口开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -2,9 +2,9 @@ package com.sky.mapper;
 
 import com.sky.dto.ShoppingCartDTO;
 import com.sky.entity.ShoppingCart;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface ShoppingCartMapper {
@@ -17,4 +17,13 @@ public interface ShoppingCartMapper {
     @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
             "values (#{name}, #{image}, #{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime})")
     int saveItem(ShoppingCart cartItem);
+
+    @Select("select * from shopping_cart where user_id = #{userId}")
+    List<ShoppingCart> listShoppingCart(Long userId);
+
+    @Delete("delete from shopping_cart where user_id = #{userId}")
+    int clearShoppingCart(Long userId);
+
+    @Delete("delete from shopping_cart where id = #{id}")
+    int deleteCartItem(Long id);
 }
--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -30,7 +31,10 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public boolean addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
         Long currentId = BaseContext.getCurrentId();
-        ShoppingCart cartItem = shoppingCartMapper.getShoppingCartItem(currentId, shoppingCartDTO);
+
+        ShoppingCart cartItem = getShoppingCartItem(currentId, shoppingCartDTO);
+        Long dishId = shoppingCartDTO.getDishId();
+        Long setmealId = shoppingCartDTO.getSetmealId();
         if (cartItem != null) {
             // 如果找到的购物车数据不为空，那么直接在原有数量上+1
             cartItem.setNumber(cartItem.getNumber() + 1);
@@ -38,12 +42,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             return affectRow > 0;
         }
         // 如果找不到购物车数据，说明是第一次添加到购物车
-        Long dishId = shoppingCartDTO.getDishId();
-        Long setmealId = shoppingCartDTO.getSetmealId();
-        if ((dishId == null && setmealId == null) || (dishId != null && setmealId != null)) {
-            // 业务判断，两个id都为空时无法添加到购物车中
-            throw new BusinessException("参数错误，别乱搞~");
-        }
         if (dishId != null) {
             // 说明添加菜品到购物车中
             Dish dish = dishMapper.getDishById(dishId);
@@ -76,5 +74,51 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         int affectRow = shoppingCartMapper.saveItem(cartItem);
 
         return affectRow > 0;
+    }
+
+    @Override
+    public List<ShoppingCart> listShoppingCart() {
+        return shoppingCartMapper.listShoppingCart(BaseContext.getCurrentId());
+    }
+
+    @Override
+    public boolean clearShoppingCart() {
+        int affectRows = shoppingCartMapper.clearShoppingCart(BaseContext.getCurrentId());
+        return affectRows >= 0;
+    }
+
+    @Override
+    public boolean subShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        Long currentId = BaseContext.getCurrentId();
+        ShoppingCart cartItem = getShoppingCartItem(currentId, shoppingCartDTO);
+        if (cartItem == null) {
+            // 购物车没有找到对应的商品数据
+            if (shoppingCartDTO.getDishId() != null) {
+                throw new BusinessException("没有找到对应的菜品ID：" + shoppingCartDTO.getDishId());
+            }
+            throw new BusinessException("没有找到对应的套餐ID：" + shoppingCartDTO.getSetmealId());
+        }
+        Integer number = cartItem.getNumber();
+        int affectRow;
+        if (number == 1) {
+            // 如果只剩下1个，那么直接删除对应的购物车数据
+            affectRow = shoppingCartMapper.deleteCartItem(cartItem.getId());
+        }else {
+            // 更新商品数量
+            cartItem.setNumber(cartItem.getNumber() - 1);
+            affectRow = shoppingCartMapper.updateCartItem(cartItem);
+        }
+        return affectRow > 0;
+    }
+
+    @Override
+    public ShoppingCart getShoppingCartItem(Long userId, ShoppingCartDTO shoppingCartDTO) {
+        Long dishId = shoppingCartDTO.getDishId();
+        Long setmealId = shoppingCartDTO.getSetmealId();
+        if ((dishId == null && setmealId == null) || (dishId != null && setmealId != null)) {
+            // 业务判断，两个id都为空时无法添加到购物车中
+            throw new BusinessException("参数错误，别乱搞~");
+        }
+        return shoppingCartMapper.getShoppingCartItem(userId, shoppingCartDTO);
     }
 }
